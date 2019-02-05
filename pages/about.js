58ec2aaec0a230bea35899f078178af887f753cc
Vinh Nguyen <quangvinh.1305@gmail.com>
2019-02-05 16:20:06 +0700

    ifx bugx

--- pages/about.js
+++ pages/about.js
@@ -22,15 +22,12 @@ class About extends React.Component {
               <div className="fadein">
                 <p>My name is Vinh Nguyen and I am an experienced software engineer and freelance developer. </p>
                 <p>
-                I have a Master's degree in Artificial Intelligence and several years of experience working
-                on a wide range of technologies and projects from C++ development for ultrasound devices to
-                 modern mobile and web applications in React and Angular.
+                I have a bachelor's degree in Artificial Intelligence and many years of experience working
+                on a wide range of technologies and projects from C++, Ruby On Rails, Node, React, Angular development for Embedded Software.
                 </p>
                 <p>
                 Throughout my career, I have acquired advanced technical knowledge and the ability to explain
-                programming topics clearly and in detail to a broad audience. I invite you to take my course,
-                where I have put a lot of effort to explain web and software engineering concepts in a detailed,
-                hands-on and understandable way.
+                programming topics clearly and in detail to a broad audience.
                 </p>
               </div>
             </Col>
